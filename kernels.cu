9bb9ca3a1181a9a2b12a7a76751a2bff9474395c
SzymonOzog <szymon.ozog@gmail.com>
2024-12-08 11:06:20 +0100

    Add naive implementation

--- kernels.cu
+++ kernels.cu
@@ -11,25 +11,36 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
-__global__ void twoSumKernel(float* nums, int target, int* out)
+__global__ void twoSumKernel(int* data, int data_num, int target, int* out)
 {
-    out[0] = 0;
-    out[1] = 1;
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < data_num)
+    {
+        int current = data[idx];
+        for (int i = idx + 1; i < data_num; i++)
+        {
+            if (current + data[i] == target)
+            {
+                out[0] = idx;
+                out[1] = i;
+            }
+        }
+    }
 }
 
-extern "C" void twoSum(float* data, int* out, int target, int data_num)
+extern "C" void twoSum(int* data, int* out, int target, int data_num)
 {
-    float* data_d;
+    int* data_d;
     int* out_d;
-    gpuErrchk(cudaMalloc((void**) &data_d, data_num*sizeof(float)));
+    gpuErrchk(cudaMalloc((void**) &data_d, data_num*sizeof(int)));
     gpuErrchk(cudaMalloc((void**) &out_d, 2*sizeof(int)));
 
-    gpuErrchk(cudaMemcpy(data_d, data, data_num*sizeof(float), cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(data_d, data, data_num*sizeof(int), cudaMemcpyHostToDevice));
 
-    dim3 grid_size(1, 1, 1);
-    dim3 block_size(1, 1, 1);
+    dim3 block_size(1024, 1, 1);
+    dim3 grid_size(ceil((float)data_num/block_size.x), 1, 1);
 
-    twoSumKernel<<<grid_size, block_size>>>(data_d, target, out_d);
+    twoSumKernel<<<grid_size, block_size>>>(data_d, data_num, target, out_d);
 
     gpuErrchk(cudaPeekAtLastError());
     gpuErrchk(cudaMemcpy(out, out_d, 2*sizeof(int), cudaMemcpyDeviceToHost));
--- kernels.cuh
+++ kernels.cuh
@@ -1,3 +1,3 @@
 #pragma once
 
-extern "C" void twoSum(float* data, int* out, int target, int data_num);
+extern "C" void twoSum(int* data, int* out, int target, int data_num);
